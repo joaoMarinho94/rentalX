05d5c6292774bc6482a7ecc167983cf9a2499d02
joaoMarinho94 <joaopaulim94@gmail.com>
2022-05-13 21:38:32 -0300

    feat: add middleware to authentication of routes

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -28,7 +28,11 @@ class UsersRepository implements IUsersRepository {
   }
 
   async findByEmail(email: string): Promise<User> {
-    return this.repository.findOne({ where: { email } });
+    return this.repository.findOne({ email });
+  }
+
+  async findById(id: string): Promise<User> {
+    return this.repository.findOne(id);
   }
 }
 
